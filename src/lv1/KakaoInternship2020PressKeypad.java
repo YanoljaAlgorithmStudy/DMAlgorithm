4b686566227e43370c922d479ef4ca1814fec225
kimdongmin <meena2003@naver.com>
2023-07-20 12:54:45 +0900

    첫 번째 커밋

---
+++ src/lv1/KakaoInternship2020PressKeypad.java
@@ -0,0 +1,12 @@
+package lv1;
+
+public class KakaoInternship2020PressKeypad {
+    public static void main(String[] args) {
+        System.out.println(solution());
+    }
+
+    public static String solution(int[] numbers, String hand) {
+
+        return "";
+    }
+}
